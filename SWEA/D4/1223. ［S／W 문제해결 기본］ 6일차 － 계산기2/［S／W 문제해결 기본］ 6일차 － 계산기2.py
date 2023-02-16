6dc462e2ab34a829931b9e055865bcbdf3c45685
Felix0942364 <122508523+Felix0942364@users.noreply.github.com>
2023-02-16 15:37:31 +0900

    [D4] Title: [S/W 문제해결 기본] 6일차 - 계산기2, Time: 151 ms, Memory: 58,524 KB -BaekjoonHub

--- SWEA/D4/1223. ［S／W 문제해결 기본］ 6일차 － 계산기2/［S／W 문제해결 기본］ 6일차 － 계산기2.py
+++ SWEA/D4/1223. ［S／W 문제해결 기본］ 6일차 － 계산기2/［S／W 문제해결 기본］ 6일차 － 계산기2.py
@@ -1,30 +1,41 @@
 for test_case in range(1, 11):
-    size = input()  # not used
-    operator = ['+', '*']
-    answer = []
+    input()
+    string = input()
+    operator = ['+', '*', '(', ')']
+    sequencing = ['+', '(', ')']
+    lst = []
     stack = []
-    for c in input():
+    for c in string:
         if c not in operator:
-            answer.append(int(c))
+            lst.append(c)
         else:
-            if c == '+':
-                while stack:
-                    answer.append(stack.pop())
-                answer.append(c)
-            if c == '*':
+            if c == ')':
+                while stack[-1] != '(':
+                    lst.append(stack.pop())
+                else:
+                    stack.pop()
+            elif c == '+' and (not stack or stack[-1] == '*'):
+                while len(stack) > 0 and stack[-1] == '*':
+                    lst.append(stack.pop())
                 stack.append(c)
-    while stack:
-        answer.append(stack.pop())
-    for i in answer:
-        if i == '*':
-            right = stack.pop()
-            left = stack.pop()
-            stack.append(left * right)
-        elif i == '+':
-            pass
-        else:
-            stack.append(i)
+            else:
+                stack.append(c)
     else:
-        answer = int(sum(stack))
+        while stack:
+            lst.append(stack.pop())
 
+    answer = int()
+    stack = list()
+    for c in lst:
+        if c not in operator:
+            stack.append(c)
+        else:
+            right = int(stack.pop())
+            left = int(stack.pop())
+            if c == '+':
+                stack.append(left+right)
+            elif c == '*':
+                stack.append(left*right)
+    else:
+        answer = stack.pop()
     print(f'#{test_case}', answer)
